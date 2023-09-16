33274302d0f275941c77119524b57f3d1fd416ea
Maksim <maximkondratyuk@gmail.com>
2023-09-16 14:38:13 +0300

    feat: add answer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  n = String(n);
+  if (n[0] === '-'){
+    n = n.slice(1);
+  }
+  let mas = n.split('');
+  let reverseMas = mas.reverse();
+  let str = reverseMas.join('');
+  let reversNumber = Number(str);
+  return reversNumber;
 }
